378c4cf1ffc731365cddcdaf6c593c88db4dd2b8
Kokou Agegee <delaliage@yahoo.fr>
2022-02-08 19:07:11 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -6,15 +6,7 @@ setInterval(timer, 1000);
 
 function timer(){
     time.innerHTML = new Date().toLocaleTimeString("fr");
-    let times = document.querySelector(".times");
     let seconds = new Date().getSeconds();
-    if(seconds % 3 === 0){
-        times.style.color = "red";
-        time.style.fontWeigth = "bold";
-    }
-    else{
-        times.style.color = "black";
-        times.style.fontWeigth = "normal";
-    }
+    time.classList.toggle("times", seconds % 3 === 0);   
 }
 
